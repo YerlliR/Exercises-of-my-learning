cd2d57538e3578e7259b39efeb24d315da5d43a1
Sergio Ricart Alabau <sergiricartt@gmail.com>
2023-10-26 23:17:40 +0200

    Delete Ejercicio 2 directory

--- Ejercicio 2/Ex4.java
+++
@@ -1,7 +0,0 @@
-
-public class Ex4 {
-    public static void main(String[] args) {
-        String Nombre =  "Sergio Ricart Alabau";
-        System.out.println("Buenos días, mi nombre es "  + Nombre);
-    }
-}
