f83d96bbccb433d9b6a1e881f7d9dc58a642a7d4
Sergio Ricart Alabau <sergiricartt@gmail.com>
2023-11-18 02:52:52 +0100

    Add files via upload

--- Exercises7/FNumDAW.java
+++ Exercises7/FNumDAW.java
@@ -1,9 +1,18 @@
+//Soy consciente de que podria haberme ahorrado mas codigo pero me he dado cuenta mientras lo hacia intentare aplicarlo para la siguiente :)
+//Soy consciente de que podria haberme ahorrado mas codigo pero me he dado cuenta mientras lo hacia intentare aplicarlo para la siguiente :)
+//Soy consciente de que podria haberme ahorrado mas codigo pero me he dado cuenta mientras lo hacia intentare aplicarlo para la siguiente :)
+//Soy consciente de que podria haberme ahorrado mas codigo pero me he dado cuenta mientras lo hacia intentare aplicarlo para la siguiente :)
+//Soy consciente de que podria haberme ahorrado mas codigo pero me he dado cuenta mientras lo hacia intentare aplicarlo para la siguiente :)
+
+
 package Exercises7;
 
-import java.util.Scanner;
+import java.util.*;
 
 public class FNumDAW {
     static Scanner scanner = new Scanner(System.in);
+    static Random random = new Random();
+
     public static boolean continuacion(){
         System.out.println("Quieres probar otro programa de la libreria? ");
         String respuesta = scanner.next();
@@ -122,4 +131,304 @@ public class FNumDAW {
         }
         return "Este numero NO es Kaprekar";
     }
+
+    public static void primosAMil(){
+        for(int x = 1; x < 1001; x = siguientePrimo(x)){
+            if (siguientePrimo(x) > 1000 ){
+                System.out.println("Estos son todos los primos entre 1 y 1000");
+                System.exit(0);
+            }
+            int numerosPrimos = siguientePrimo(x);
+            System.out.println(numerosPrimos);
+        }
+    }
+
+    public static void capicuaANueve(){
+        for(int i = 1000; i < 9999; i++){
+            if (esCapicua(i)){
+                System.out.println(i);
+            }
+        }
+    }
+
+    public static void primoMenorDePalabra(int repeticiones){
+        int[] acabadoPrint = new int[repeticiones];
+
+        for (int i  = 0; i < repeticiones; i++ ){
+
+            String palabra = scanner.next();
+            char[] separado = palabra.toCharArray();
+            int ascii = 0;
+
+            for (int x = 0; x < separado.length; x++) {
+                ascii = ascii + (int) separado[x];
+            }
+            acabadoPrint[i] = ascii;
+        }
+
+        for (int i = 0; i < repeticiones; ++i){
+            System.out.println(acabadoPrint[i]);
+        }
+
+    }
+
+    public static int[] generaArrayInt(int tamanyoArray, int min, int max, String imprimir){
+        int[] randomArray = new int[tamanyoArray];
+
+        for (int x = 0; x < tamanyoArray; x++){
+            randomArray[x] = random.nextInt(max - min + 1) + min;
+        }
+
+        if (imprimir.equalsIgnoreCase("Si")){
+            imprimirArray(tamanyoArray,randomArray);
+        }
+        return randomArray;
+
+    }
+
+    public static void imprimirArray(int tamanyoArray, int[] randomArray){
+        for (int x = 0 ; x < tamanyoArray; x++){
+            System.out.print(randomArray[x] + ", ");
+        }
+        System.out.println();
+    }
+
+    public static void minimoArrayInt(int tamanyoArray, String randomArraySiNo){
+
+        int menor = 2147483647;
+
+        if (randomArraySiNo.equalsIgnoreCase("Si")){
+            int[] arraysNums = new int[tamanyoArray];
+            for (int x = 0; x < tamanyoArray; x++){
+                System.out.println("Dime la posicion " + x + " de la array");
+                arraysNums[x] = scanner.nextInt();
+
+                menor = arraysNums[0];
+
+                for (int i = 0 ; i < tamanyoArray; i++){
+                    if (menor > arraysNums[i]){
+                        menor = arraysNums[i];
+                    }
+                }
+            }
+
+        }else {
+
+            System.out.println("Dime el numero mas pequeño que quieres que se almacenen en la array");
+            int randomMin = scanner.nextInt();
+
+            System.out.println("Dime el numero mas grande que quieres que se almacenen en la array");
+            int randomMax = scanner.nextInt();
+
+            System.out.println("Quieres imprimir la array?");
+            String imprimir = scanner.next();
+
+            int[] arrayGenerada = generaArrayInt(tamanyoArray, randomMin, randomMax, imprimir);
+
+            menor = arrayGenerada[0];
+
+            for (int i = 0 ; i < tamanyoArray; i++){
+                if (menor > arrayGenerada[i]){
+                    menor = arrayGenerada[i];
+                }
+            }
+        }
+
+        System.out.println("El numero mas pequeño de la array es: " + menor);
+
+    }
+
+    public static void maximoArrayInt(int tamanyoArray, String randomArraySiNo){
+
+        int mayor = -2147483648;
+
+        if (randomArraySiNo.equalsIgnoreCase("Si")){
+            int[] arraysNums = new int[tamanyoArray];
+            for (int x = 0; x < tamanyoArray; x++){
+                System.out.println("Dime la posicion " + x + " de la array");
+                arraysNums[x] = scanner.nextInt();
+
+                mayor = arraysNums[0];
+
+                for (int i = 0 ; i < tamanyoArray; i++){
+                    if (mayor < arraysNums[i]){
+                        mayor = arraysNums[i];
+                    }
+                }
+            }
+
+        }else {
+
+            System.out.println("Dime el numero mas pequeño que quieres que se almacenen en la array");
+            int randomMin = scanner.nextInt();
+
+            System.out.println("Dime el numero mas grande que quieres que se almacenen en la array");
+            int randomMax = scanner.nextInt();
+
+            System.out.println("Quieres imprimir la array?");
+            String imprimir = scanner.next();
+
+            int[] arrayGenerada = generaArrayInt(tamanyoArray, randomMin, randomMax, imprimir);
+
+            mayor = arrayGenerada[0];
+
+            for (int i = 0 ; i < tamanyoArray; i++){
+                if (mayor < arrayGenerada[i]){
+                    mayor = arrayGenerada[i];
+                }
+            }
+        }
+
+        System.out.println("El numero mas pequeño de la array es: " + mayor);
+    }
+
+    public static void mediaArrayInt(int tamanyoArray, String randomArraySiNo){
+
+        if (randomArraySiNo.equalsIgnoreCase("Si")){
+            int[] arraysNums = new int[tamanyoArray];
+
+            for (int x = 0; x < tamanyoArray; x++){
+                System.out.println("Dime la posicion " + x + " de la array");
+                arraysNums[x] = scanner.nextInt();
+            }
+
+            System.out.println("La media de la array es: " + sacarMedia(arraysNums));
+        }else {
+
+            System.out.println("Dime el numero mas pequeño que quieres que se almacenen en la array");
+            int randomMin = scanner.nextInt();
+
+            System.out.println("Dime el numero mas grande que quieres que se almacenen en la array");
+            int randomMax = scanner.nextInt();
+
+            System.out.println("Quieres imprimir la array?");
+            String imprimir = scanner.next();
+
+            int[] arrayGenerada = generaArrayInt(tamanyoArray, randomMin, randomMax, imprimir);
+            System.out.println("La media de la array es: " + sacarMedia(arrayGenerada));
+        }
+
+
+    }
+
+    public static int sacarMedia(int[] datos){
+        int numeroFinalizado = 0;
+        for (int i = 0; i < datos.length; i++){
+            numeroFinalizado = numeroFinalizado + datos[i];
+        }
+        numeroFinalizado = numeroFinalizado / datos.length;
+        return numeroFinalizado;
+    }
+
+    public static void estaEnArrayInt(int tamanyoArray, String randomArraySiNo, int numeroABuscar){
+
+
+        if (randomArraySiNo.equalsIgnoreCase("Si")){
+            int[] arraysNums = new int[tamanyoArray];
+
+            for (int x = 0; x < tamanyoArray; x++){
+                System.out.println("Dime la posicion " + x + " de la array");
+                arraysNums[x] = scanner.nextInt();
+            }
+            System.out.println(buscarNumero(arraysNums,numeroABuscar,true));
+        }else {
+
+            System.out.println("Dime el numero mas pequeño que quieres que se almacenen en la array");
+            int randomMin = scanner.nextInt();
+
+            System.out.println("Dime el numero mas grande que quieres que se almacenen en la array");
+            int randomMax = scanner.nextInt();
+
+            System.out.println("Quieres imprimir la array?");
+            String imprimir = scanner.next();
+
+            int[] arrayGenerada = generaArrayInt(tamanyoArray, randomMin, randomMax, imprimir);
+            System.out.println(buscarNumero(arrayGenerada,numeroABuscar,true));
+        }
+
+    }
+
+    public static String buscarNumero(int[] enteros, int numerABuscar, boolean resultado){
+        for (int x = 0 ; x < enteros.length; x++){
+            if (numerABuscar == enteros[x]){
+                if (resultado){
+                    return "Este numero se encuentra en la array";
+                }else {
+                    return "El numero: " + numerABuscar + " se encuentra en la posicion: " + x ;
+                }
+
+            }
+        }
+        return "NO esta este numero en la array";
+    }
+
+    public static void posicionEnArray(int tamanyoArray, String randomArraySiNo, int numeroABuscar){
+
+
+        if (randomArraySiNo.equalsIgnoreCase("Si")){
+            int[] arraysNums = new int[tamanyoArray];
+
+            for (int x = 0; x < tamanyoArray; x++){
+                System.out.println("Dime la posicion " + x + " de la array");
+                arraysNums[x] = scanner.nextInt();
+            }
+            System.out.println(buscarNumero(arraysNums,numeroABuscar,true));
+        }else {
+
+            System.out.println("Dime el numero mas pequeño que quieres que se almacenen en la array");
+            int randomMin = scanner.nextInt();
+
+            System.out.println("Dime el numero mas grande que quieres que se almacenen en la array");
+            int randomMax = scanner.nextInt();
+
+            System.out.println("Quieres imprimir la array?");
+            String imprimir = scanner.next();
+
+            int[] arrayGenerada = generaArrayInt(tamanyoArray, randomMin, randomMax, imprimir);
+            System.out.println(buscarNumero(arrayGenerada,numeroABuscar,true));
+        }
+
+    }
+
+    public static void volteaArrayInt(int tamanyoArray, String randomArraySiNo){
+
+
+        if (randomArraySiNo.equalsIgnoreCase("Si")){
+            int[] arraysNums = new int[tamanyoArray];
+
+            for (int x = 0; x < tamanyoArray; x++){
+                System.out.println("Dime la posicion " + x + " de la array");
+                arraysNums[x] = scanner.nextInt();
+            }
+            System.out.println("Esta es la array volteada: ");
+            imprimirArray(tamanyoArray, rotarArray(arraysNums));
+
+        }else {
+
+            System.out.println("Dime el numero mas pequeño que quieres que se almacenen en la array");
+            int randomMin = scanner.nextInt();
+
+            System.out.println("Dime el numero mas grande que quieres que se almacenen en la array");
+            int randomMax = scanner.nextInt();
+
+            System.out.println("Quieres imprimir la array?");
+            String imprimir = scanner.next();
+
+            int[] arrayGenerada = generaArrayInt(tamanyoArray, randomMin, randomMax, imprimir);
+
+            System.out.println("Esta es la array volteada: ");
+            imprimirArray(tamanyoArray, rotarArray(arrayGenerada));
+        }
+
+    }
+
+    public static int[] rotarArray(int[] array){
+        for (int i = 0; i < array.length / 2; i++) {
+            int temp = array[i];
+            array[i] = array[array.length - i - 1];
+            array[array.length - i - 1] = temp;
+        }
+        return array;
+    }
+
 }
--- Exercises7/Main.java
+++ Exercises7/Main.java
@@ -1,5 +1,11 @@
+//Soy consciente de que podria haberme ahorrado mas codigo pero me he dado cuenta mientras lo hacia intentare aplicarlo para la siguiente :)
+//Soy consciente de que podria haberme ahorrado mas codigo pero me he dado cuenta mientras lo hacia intentare aplicarlo para la siguiente :)
+//Soy consciente de que podria haberme ahorrado mas codigo pero me he dado cuenta mientras lo hacia intentare aplicarlo para la siguiente :)
+//Soy consciente de que podria haberme ahorrado mas codigo pero me he dado cuenta mientras lo hacia intentare aplicarlo para la siguiente :)
+//Soy consciente de que podria haberme ahorrado mas codigo pero me he dado cuenta mientras lo hacia intentare aplicarlo para la siguiente :)
 package Exercises7;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
@@ -12,18 +18,29 @@ public class Main {
 
         System.out.println(FNumDAW.esPrimo(7));
         System.out.println("Esta es la lista de mini porgramas que tenemos:");
-        System.out.println( "1.  Calculoar potencias                 \n" +
-                            "2.  Contar longitud de numeros          \n" +
-                            "3.  Voltear un numero                   \n" +
-                            "4.  Saber si un numero es capicua       \n" +
-                            "5.  Numero primo                        \n" +
-                            "6.  Siguiente primo                     \n" +
-                            "7.  Anterior primo                      \n" +
-                            "8.  Extraccion de posicion de un numero \n" +
-                            "9.  Busca numeros                       \n" +
-                            "10. Trozo de numero                     \n" +
-                            "11. Juntar dos numeros                  \n" +
-                            "12. Kaprekar                            \n"
+        System.out.println( "1.  Calculoar potencias                        \n" +
+                            "2.  Contar longitud de numeros                 \n" +
+                            "3.  Voltear un numero                          \n" +
+                            "4.  Saber si un numero es capicua              \n" +
+                            "5.  Numero primo                               \n" +
+                            "6.  Siguiente primo                            \n" +
+                            "7.  Anterior primo                             \n" +
+                            "8.  Extraccion de posicion de un numero        \n" +
+                            "9.  Busca numeros                              \n" +
+                            "10. Trozo de numero                            \n" +
+                            "11. Juntar dos numeros                         \n" +
+                            "12. Kaprekar                                   \n" +
+                            "13. Numeros primos de 1 a 1000                 \n" +
+                            "14. Numeros capicua de 1000 a 9999             \n" +
+                            "15. Primo menor de palabra en ASCII            \n" +
+                            "16. \n" +
+                            "17. Generador de arrays                        \n" +
+                            "18. Buscar el numero mas pequeño de una array  \n" +
+                            "19. Buscar el numero mas grande de una array   \n" +
+                            "20. Hacer la media de los numeros de una array \n" +
+                            "21. Detector de valor en Array                 \n" +
+                            "22. Buscar el numero en Array                  \n" +
+                            "19. Voltear Array                              \n"
         );
 
         while(continuacion){
@@ -159,7 +176,111 @@ public class Main {
                     System.out.println(FNumDAW.kaprekar(numerito));
 
                     continuacion = FNumDAW.continuacion();
-                    break;
+                break;
+
+                case 13:
+                    System.out.println("Estos son todos los numeros primos de 1 a 1000");
+
+                    FNumDAW.primosAMil();
+
+                    continuacion = FNumDAW.continuacion();
+                break;
+
+                case 14:
+                    System.out.println("Estos son todos los numeros capicua de 1000 a 9999");
+
+                    FNumDAW.capicuaANueve();
+
+                    continuacion = FNumDAW.continuacion();
+                break;
+
+                case 15:
+                    System.out.println("Dime cuantas palabras / caracteres vas a querer saber su primo menor en asci");
+                    int veces = scanner.nextInt();
+
+                    FNumDAW.primoMenorDePalabra(veces);
+
+                    continuacion = FNumDAW.continuacion();
+                break;
+
+                case 16:
+                    /*
+                    System.out.println("Dime cuantas palabras / caracteres vas a querer saber su primo menor en asci");
+                    int veces = scanner.nextInt();
+
+                    FNumDAW.primoMenorDePalabra(veces);
+
+                    continuacion = FNumDAW.continuacion();*/
+                break;
+
+                case 17:
+                    System.out.println("Dime el tamaño de la array de randoms");
+                    int tamanyArray = scanner.nextInt();
+
+                    System.out.println("Dime el numero mas pequeño que quieres que se almacenen en la array");
+                    int randomMin = scanner.nextInt();
+
+                    System.out.println("Dime el numero mas grande que quieres que se almacenen en la array");
+                    int randomMax = scanner.nextInt();
+
+                    System.out.println("Quieres imprimir la array?");
+                    String imprimir = scanner.next();
+
+                    FNumDAW.generaArrayInt(tamanyArray, randomMin, randomMax, imprimir);
+
+                    continuacion = FNumDAW.continuacion();
+                break;
+
+                case 18, 19, 20:
+                    System.out.println("Dime el tamaño de la array");
+                    int tamanyArray2 = scanner.nextInt();
+
+                    System.out.println("Quieres seleccionar tu los numeros de la array ?");
+                    String seleccion = scanner.next();
+
+                    if (eleccion == 18){
+                        FNumDAW.minimoArrayInt(tamanyArray2,seleccion);
+                    } else if (eleccion == 19) {
+                        FNumDAW.maximoArrayInt(tamanyArray2, seleccion);
+                    } else if (eleccion == 20) {
+                        FNumDAW.mediaArrayInt(tamanyArray2, seleccion);
+                    }
+
+                    continuacion = FNumDAW.continuacion();
+                break;
+
+                case 21, 22:
+                    System.out.println("Dime el tamaño de la array");
+                    int tamanyArray5 = scanner.nextInt();
+
+                    System.out.println("Quieres seleccionar tu los numeros de la array ?");
+                    String seleccion2 = scanner.next();
+
+                    System.out.println("Dime el numero que quieres buscar en la array: ");
+                    int numeroBuscar = scanner.nextInt();
+
+                    if (eleccion == 21){
+                        FNumDAW.estaEnArrayInt(tamanyArray5, seleccion2, numeroBuscar);
+                    } else if (eleccion == 22) {
+                        FNumDAW.posicionEnArray(tamanyArray5, seleccion2, numeroBuscar);
+                    }
+
+
+                    continuacion = FNumDAW.continuacion();
+                break;
+
+                case 23:
+                    System.out.println("Dime el tamaño de la array");
+                    int tamanyArray6 = scanner.nextInt();
+
+                    System.out.println("Quieres seleccionar tu los numeros de la array ?");
+                    String arrayNoSi = scanner.next();
+
+                    FNumDAW.volteaArrayInt(tamanyArray6, arrayNoSi);
+
+                    continuacion = FNumDAW.continuacion();
+                break;
+
             }
         }
     }
