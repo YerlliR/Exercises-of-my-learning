c7edc5c769725f1e81061d01a0874cf9914a7710
Sergio Ricart Alabau <sergiricartt@gmail.com>
2024-01-23 22:52:19 +0100

    Update EstimacionSaldos.java

--- Exercises12/EstimacionSaldos.java
+++ Exercises12/EstimacionSaldos.java
@@ -1,40 +1,55 @@
+import java.util.Queue;
+import java.util.LinkedList;
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class EstimacionSaldos {
-
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        ArrayList<Queue<Integer>> colasLadrones = new ArrayList<>();
+        
+        boolean enEjecucion = true;
+        Integer totalBilletes;
 
-        System.out.println("Dime la cantidad inicial y el número de movimientos bancarios (separados por espacio):");
-        String[] inicio = scanner.nextLine().split(" ");
-        int cantidadInicial = Integer.parseInt(inicio[0]);
-        int numMovimientos = Integer.parseInt(inicio[1]);
+        while (enEjecucion) {
+            int numBilletes = scanner.nextInt();
+            int numLadrones = scanner.nextInt();
 
-        int[] saldos = new int[12];
-        saldos[0] = cantidadInicial;
-
-        for (int i = 0; i < numMovimientos; i++) {
-            System.out.println("Dime la fecha (DD-MM), la cantidad y el concepto (separados por espacio):");
-            String[] movimiento = scanner.nextLine().split(" ");
-            String[] fecha = movimiento[0].split("-");
-            int dia = Integer.parseInt(fecha[0]);
-            int mes = Integer.parseInt(fecha[1]);
-            int cantidad = Integer.parseInt(movimiento[1]);
-
-            int indiceMes = mes - 1;
-
-            if (cantidad > 0) {
-                saldos[indiceMes] += cantidad;
+            if (numLadrones == 0) {
+                enEjecucion = false;
             } else {
-                if (indiceMes >= i) {
-                    saldos[indiceMes] += cantidad;
+                for (int i = 0; i < numLadrones; i++) {
+                    Queue<Integer> colaBilletes = new LinkedList<>();
+                    colasLadrones.add(colaBilletes);
                 }
+
+                int indiceLadron = 0;
+                for (int i = 0; i < numBilletes; i++) {
+                    colasLadrones.get(indiceLadron).add(scanner.nextInt());
+                    indiceLadron = (indiceLadron == colasLadrones.size() - 1) ? 0 : indiceLadron + 1;
+                }
+
+                for (int i = 0; i < numLadrones; i++) {
+                    totalBilletes = 0;
+                    Queue<Integer> colaBilletesLadron = new LinkedList<>(colasLadrones.get(i));
+
+                    for (Integer billete : colaBilletesLadron) {
+                        totalBilletes += billete;
+                    }
+
+                    System.out.print(totalBilletes + " : ");
+                    for (Integer billete : colaBilletesLadron) {
+                        System.out.print(billete + " ");
+                    }
+                    System.out.println("");
+                }
+
+                System.out.println("--");
+
+                colasLadrones.clear();
             }
         }
 
-        System.out.println("Saldo al final de cada mes:");
-        for (int saldo : saldos) {
-            System.out.print(saldo + " ");
-        }
+        scanner.close();
     }
 }
