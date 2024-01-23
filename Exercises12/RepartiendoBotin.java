7c23dd5e32c7e63e29d06e9fb1c78164c4c22478
Sergio Ricart Alabau <sergiricartt@gmail.com>
2024-01-23 22:50:22 +0100

    Update RepartiendoBotin.java

--- Exercises12/RepartiendoBotin.java
+++ Exercises12/RepartiendoBotin.java
@@ -3,53 +3,50 @@ import java.util.LinkedList;
 import java.util.Scanner;
 import java.util.ArrayList;
 
-public class RepartiendoBotin{
+public class RepartirBotin {
     public static void main(String[] args) {
-        Scanner lectura = new Scanner(System.in);
-        ArrayList<Queue<Integer>> jefe = new ArrayList<Queue<Integer>>();
-        boolean bucle = true ;
-        Integer numTot;
-        while (bucle == true){
-            int NumBill = lectura.nextInt();
-            int cont = 0 ;
-            int NumAtrac = lectura.nextInt();
-            //crear cuentas atracadores
-            for (int i = 0; i <NumAtrac;i++){
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<Queue<Integer>> colasLadrones = new ArrayList<>();
+        
+        boolean enEjecucion = true;
+        Integer totalBilletes;
 
-                Queue<Integer> billetesi = new LinkedList<Integer>();
-                jefe.add(billetesi);
-            }
-            //salir del programa
-            if (NumAtrac ==  0){
-                bucle = false;
-            }
-            //Reparte billetes
-            for (int i = 0;cont<NumBill;cont++){
-                jefe.get(i).add(lectura.nextInt());
-                if (i == jefe.size() - 1) {
-                    i = 0;
-                } else {
-                    i++;
-                }
-            }
-            //printear los numeros
-            for (int i = 0;i<NumAtrac;i++){
-                numTot = 0 ;
-                Queue<Integer> billetes1 = new LinkedList<Integer>(jefe.get(i));
-                for (Integer num : billetes1) {
-                    numTot += num;
-                }
-                System.out.print(numTot+" : ");
-                for (Integer num : billetes1) {
-                    System.out.print(num+" ");
-                }System.out.println("");
-            }
-            System.out.println("--");
+        while (enEjecucion) {
+            int numBilletes = scanner.nextInt();
+            int numLadrones = scanner.nextInt();
 
-            jefe.clear();
+            if (numLadrones == 0) {
+                enEjecucion = false;
+            } else {
+                for (int i = 0; i < numLadrones; i++) {
+                    Queue<Integer> colaBilletes = new LinkedList<>();
+                    colasLadrones.add(colaBilletes);
+                }
+
+                int indiceLadron = 0;
+                for (int i = 0; i < numBilletes; i++) {
+                    colasLadrones.get(indiceLadron).add(scanner.nextInt());
+                    indiceLadron = (indiceLadron == colasLadrones.size() - 1) ? 0 : indiceLadron + 1;
+                }
+
+                for (int i = 0; i < numLadrones; i++) {
+                    totalBilletes = 0;
+                    Queue<Integer> colaBilletesLadron = new LinkedList<>(colasLadrones.get(i));
+
+                    for (Integer billete : colaBilletesLadron) {
+                        totalBilletes += billete;
+                    }
+
+                    System.out.print(totalBilletes + " : ");
+                    for (Integer billete : colaBilletesLadron) {
+                        System.out.print(billete + " ");
+                    }
+                    System.out.println("");
+                }
+                System.out.println("--");
+                colasLadrones.clear();
+            }
         }
-
-        lectura.close();
 
     }
 }
